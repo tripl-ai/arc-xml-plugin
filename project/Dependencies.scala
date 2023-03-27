7b58c3670b498f126da6dad27f97b76374dfc2ea
John Bruce <john.a.m.bruce@gmail.com>
2023-03-26 20:20:53 -0400

    Release 1.5.0 (#3)
     Update to Arc 3.13.2 and spark-xml 0.16.0
    
    ---------
    
    Co-authored-by: Mike Seddon <seddonm1@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,19 +2,19 @@ import sbt._
 
 object Dependencies {
   // versions
-  lazy val sparkVersion = "3.0.1"
+  lazy val sparkVersion = "3.3.2"
 
   // testing
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7" % "test,it"
 
   // arc
-  val arc = "ai.tripl" %% "arc" % "3.7.0" % "provided"
+  val arc = "ai.tripl" %% "arc" % "3.13.2" % "provided"
 
   // spark
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
 
   // spark XML
-  val sparkXML = "com.databricks" %% "spark-xml" % "0.9.0"
+  val sparkXML = "com.databricks" %% "spark-xml" % "0.16.0"
 
   // Project
   val etlDeps = Seq(
